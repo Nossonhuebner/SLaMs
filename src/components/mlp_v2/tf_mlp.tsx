4f83c41650fa79e27a4b3d40c894887c3c23a6bf
Nosson Huebner <38170695+Nossonhuebner@users.noreply.github.com>
2023-12-13 18:44:00 +0000

    V2 (TF) working

--- src/components/mlp_v2/tf_mlp.tsx
+++ src/components/mlp_v2/tf_mlp.tsx
@@ -1,7 +1,7 @@
 import { clean } from "../../util"
 import English from '../../../raw_english'
 import * as tf from '@tensorflow/tfjs';
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { Button } from "@mui/material";                 
 
 class TokenizerLite {
@@ -31,54 +31,67 @@ class TokenizerLite {
     }
 }
 
+const embeddingSize = 30; // Size of the output embedding vector
+const contextLength = 5; // Length of each input sequence ()
+const batchSize = 640; // Number of sequences in a batch
+
+const lr = 0.001
+
 function TfMlp() {
     const tokenizer = new TokenizerLite();
     const cleaned = clean(English);
-    const tokens = tokenizer.encode(cleaned);
+    console.log(cleaned.length)
+    const sample = cleaned.slice(0, 30000);
+    const tokens = tokenizer.encode(sample);
     const [generated, setGenerated] = useState<string[]>([]);
 
     const inputDim = tokenizer.vocab.length+1 // Size of the vocabulary
-    const embeddingSize = 2; // Size of the output embedding vector
-    const contextLength = 3; // Length of each input sequence ()
-    const batchSize = 64; // Number of sequences in a batch
+
 
     const d = createDataset2(tokens, contextLength)
     tf.util.shuffle(d);
     const inn = d.map(item => item.input);
     const out = d.map(item => item.output);
 
-    const model = tf.sequential();
+    const model = useMemo(() => {
+        const m = tf.sequential();
 
-    // Add an embedding layer
-    model.add(tf.layers.embedding({
-        inputDim: inputDim, // vocabulary size
-        outputDim: embeddingSize, // embedding size
-        inputLength: contextLength // context size
-    }));
+        // Add an embedding layer
+        m.add(tf.layers.embedding({
+            inputDim: inputDim, // vocabulary size
+            outputDim: embeddingSize, // embedding size
+            inputLength: contextLength // context size
+        }));
 
-    // Flatten the output from the embedding layer
-    model.add(tf.layers.flatten());
-    model.add(tf.layers.dense({ units: 64, activation: 'relu' }));
-    model.add(tf.layers.dense({ units: 28, activation: 'softmax' }));
+        // Flatten the output from the embedding layer
+        m.add(tf.layers.flatten());
+        m.add(tf.layers.dense({ units: 64, activation: 'relu' }));
+        m.add(tf.layers.dense({ units: 28, activation: 'softmax' }));
 
 
-    model.compile({
-        optimizer: 'adam',
-        loss: 'categoricalCrossentropy',
-        metrics: ['accuracy']
-      });
+        m.compile({
+            optimizer:  tf.train.adam(lr) ,
+            loss: 'categoricalCrossentropy',
+            metrics: ['accuracy']
+        });
+
+        return m;
+    }, [inputDim, embeddingSize, contextLength]);
+
+
 
     model.summary()
     const oneHotLabels = tf.oneHot(out, tokenizer.vocab.length);
 
     const train = () => {
         model.fit(tf.tensor2d(inn, [inn.length, contextLength]), oneHotLabels, {
-            epochs: 50,        // Number of epochs to train the model
+            epochs: 30,        // Number of epochs to train the model
             validationSplit: 0.2,  // Fraction of the training data to be used as validation data
             batchSize: batchSize,
             callbacks: {       // Callbacks to execute during training
               onEpochEnd: (epoch, logs) => {
-                console.log(`Epoch ${epoch + 1}: Loss: ${logs?.loss}, Accuracy: ${logs?.accuracy}`);
+                console.log(logs)
+                console.log(`Epoch ${epoch + 1}: Loss: ${logs?.loss}, Val_Accuracy: ${logs?.val_acc}`);
               }
             }
         });
@@ -104,7 +117,7 @@ function predictWords(tokenizer: TokenizerLite, model: tf.Sequential, count: num
 
 function predictWord(tokenizer: TokenizerLite, model: tf.Sequential) {
     let output = ''
-    let input = "***"
+    let input = "*".repeat(contextLength)
     const result = []
     while (output !== '*') {
         result.push(output)
@@ -117,7 +130,7 @@ function predictWord(tokenizer: TokenizerLite, model: tf.Sequential) {
 
 function predict(str: string, tokenizer: TokenizerLite, model: tf.Sequential) {
     const enc = tokenizer.encode(str);
-    const inputTensor = tf.tensor2d([enc], [1, 3]);
+    const inputTensor = tf.tensor2d([enc], [1, contextLength]);
     const prediction = model.predict(inputTensor) as tf.Tensor;  
     const values = prediction.arraySync() as number[][];
     
