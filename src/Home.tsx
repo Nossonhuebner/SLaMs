066defba30415e177a02be6818e19b41144de29c
Nosson Huebner <38170695+Nossonhuebner@users.noreply.github.com>
2023-12-19 03:00:25 +0000

    Import readme

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import readme from '../README.md';
 
 const HomePage: React.FC = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <ReactMarkdown children={readme} />
   );
 };
 
